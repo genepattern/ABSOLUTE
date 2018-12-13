97fa3ac6e1ba70dcee5e70c18bbf839f1f3d9ee9
ted Liefeld <liefeld@broadinstitute.org>
2018-12-13 14:05:42 -0800

    adding license to top level directory

--- docker/dockerLoadLibrary.R
+++ docker/dockerLoadLibrary.R
@@ -1,18 +1,4 @@
-libdir = '/usr/local/bin/ABSOLUTE/'
-patch.dir = '/patches'
-server.dir='/usr/local/lib/R'
-vers="2.15"
 absol.library="/patches/rlib/2.15/site-library"
-.libPaths(c(absol.library, .libPaths()))
-
-source(file.path(libdir, "loadRLibrary.R"))
-load.packages(libdir, patch.dir, server.dir, vers)
 
 suppressPackageStartupMessages(library("ABSOLUTE", lib.loc = absol.library, character.only=TRUE))
-
-
-
-
-
-
 
